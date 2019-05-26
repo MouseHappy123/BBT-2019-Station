332db132a974fe75071da4bd49ad0822f7e681f6
MouseHappy123 <1351923644@qq.com>
2019-05-26 22:05:11 +0800

    migrations

--- Backend/database/migrations/2019_05_08_155307_create_station_table.php
+++ Backend/database/migrations/2019_05_08_155307_create_station_table.php
@@ -11,13 +11,14 @@ class CreateStationTable extends Migration
      */
     public function up()
     {
-        Schema::create('station1', function (Blueprint $table) {
+        Schema::create('station', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('openid', 100);
             $table->string('passenger1', 10);
             $table->string('passenger2', 10)->default('NULL');
             $table->string('passenger3', 10)->default('NULL');
             $table->string('destination', 20);
-            $table->text('comment');
+            $table->string('comment', 50);
             $table->time('created_at')->default('NULL');
             $table->time('updated_at')->default('NULL');
         });
